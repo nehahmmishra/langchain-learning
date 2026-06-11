b2fb20f0295a7618283c89b45c6c7ae455adcc0d
Neha Mishra <91901958+nehahmmishra@users.noreply.github.com>
2026-06-11 06:55:04 +0530

    Create RecursiveCharacterTextSplitter.py

---
+++ RecursiveCharacterTextSplitter.py
@@ -0,0 +1,13 @@
+#structure based text splitting
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+text=""" Artificial intelligence firm Anthropic has launched a $200 million initiative to examine how AI will reshape employment and economic growth, while urging governments to prepare for potentially severe labour market disruptions as the technology becomes more powerful.
+The announcement was accompanied by a new policy framework and a detailed essay from Anthropic chief executive Dario Amodei, who argued that AI could transform the workforce more rapidly and more extensively than previous technological revolutions.
+
+Amodei said policymakers and businesses should begin preparing now for scenarios in which automation significantly reduces demand for human labour.
+
+The key challenge in such a world won't be incentivizing growth, but finding a way for everyone to s...
+"""
+splitter = RecursiveCharacterTextSplitter(chunk_size=250,chunk_overlap=0)
+chunks = splitter.split_text(text)
+print (len(chunks))
+print(chunks)
